ed35e07c62a10cb570a71691e83118b577360980
Anthony McCofie <AnthonyTheOracle@outlook.com>
2022-01-28 15:32:16 -0500

    updates made @ 3:32:54PM

---
+++ Assignment3.R
@@ -0,0 +1,39 @@
+# - Module # 3 Introduction to Data.frame
+
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
+ABC <- c(4, 62, 51, 21, 2, 14, 15)
+CBS <- c(12, 75, 43, 19, 1, 21, 19)
+
+polls <- cbind(Name, ABC, CBS)
+
+polls
+
+#     Name      ABC  CBS 
+#[1,] "Jeb"     "4"  "12"
+#[2,] "Donald"  "62" "75"
+#[3,] "Ted"     "51" "43"
+#[4,] "Marco"   "21" "19"
+#[5,] "Carly"   "2"  "1" 
+#[6,] "Hillary" "14" "21"
+#[7,] "Berine"  "15" "19"
+
+# using cbind as shown above, it looks like numeric values are treated as characters
+# making is impossible to apply descriptive statistics functions
+
+#updating the above script with data.frame data structure
+
+polls.df <- data.frame(Name, ABC, CBS)
+polls.df
+
+#The issue with numeric was solved with the use of data.frame
+#> polls.df
+##    Name ABC CBS
+#1     Jeb   4  12
+#2  Donald  62  75
+#3     Ted  51  43
+#4   Marco  21  19
+#5   Carly   2   1
+#6 Hillary  14  21
+#7  Berine  15  19
+
+
